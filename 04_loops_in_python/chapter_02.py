b947ee9552d5e05d812146264e922abf756d96d4
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-09 16:24:36 +0530

    add walrus operator in python

---
+++ 04_loops_in_python/chapter_02.py
@@ -0,0 +1,18 @@
+# This file will hold the knowledge about the newly not newly but yeah a new operator known as walrus operator...
+
+# walrus operator...
+
+# := this is known as the walrus operator.
+
+# as we know that this is a statement.
+# x = 3 this doesn't returns any result to us.
+# 4 + 5 this is an expression! which returns us the result...
+
+# walrus is something, which lets you assign and evaluate a variable at the same time...
+
+value = 12
+
+# remainder = 12 % 5
+
+if remainder:= value % 5:
+    print(f"Not divisible, remainder is:{remainder}")
