ac13990e21da9f909453583a1882693bda43b0c3
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-09 15:58:56 +0530

    add loops in python

--- 04_loops_in_python/chapter_01.py
+++ 04_loops_in_python/chapter_01.py
@@ -12,23 +12,67 @@
 # In python for loop iterates only on the iterables, you dont need to manually increment or initialise any value...
 
 
-# for token in range(1,11):
-#     print(f"Serving chai to token #{token}")
+for token in range(1,11):
+    print(f"Serving chai to token #{token}")
 
 # here range() is an iterable -> a sequence Object which generates number from a specified range to a specified range where the last_range is exclusive.
 # Also note that, range()'s third parameter is step - so it skips the specified number of steps.
 
-# for number in range(1,11,2):
-#     print(number)
+for number in range(1,11,2):
+    print(number)
 
 
 
 names = ("Shailesh","Nilesh","Megha", "Sonali")
-# for users in names:
-#     print(users)
+for users in names:
+    print(users)
 
 
 # Now lets move towards enumerate() it helps you to print the index number.
 
 for idx, user in enumerate(names, start=1):
     print(f"{idx}: {user}")
+
+
+# ls = list(range(1,11,2))
+# print(ls)
+
+names = ("Shailesh","Ravi","Pavan","Max")
+bills = [50,45,60, 75]
+
+for name,amount in zip(names,bills):
+    print(f"{name} paid {amount} rupees")
+
+# for name in names:
+#     print(name)
+
+# print(names)
+
+zippedOne = zip(names,bills)
+# print(zippedOne)
+
+# print(list(zippedOne))
+
+# lets understand while loop in python...
+# while loop provides control in python, it lets you control the loop using break and continue clause/statement.
+
+# lets see an example:
+
+num = 1
+while num<=10:
+    print(num)
+    num+=1
+
+# controlling the flow using break and continue.
+
+while num <= 10:
+    if num ==3:
+        num+=1
+        continue
+    print(num)
+    num+=1
+
+# Also know that theres a fallback as else in for loop.
+
+
+
