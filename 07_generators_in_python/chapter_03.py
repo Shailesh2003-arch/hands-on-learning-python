288301ff7cc4c9009b14d0427756c8c7b6cbd9a8
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-11 22:01:13 +0530

    add sending value to generator in python into the codebase

---
+++ 07_generators_in_python/chapter_03.py
@@ -0,0 +1,23 @@
+# Now this file will hold the knowledge about sending data to the generator.
+# Yess!... you can send data to the generators.
+
+
+# lets define a simple generator function...
+
+
+def chai_customer():
+    print ("Welcome ! what type of chai would you like to have ?") # it executes this line
+    print("Print kar raha hu aisehii, maja aaraha hai 😂😂")
+    print("Print kar raha hu aisehii, maja aaraha hai 😂😂")
+    order = yield # and waits for any value to be recieved here. if not recieved, then it terminates.
+    while True: # loop - for contiuous (infinite generation of value)
+        print(f"Preparing {order} for you!...")
+        order = yield
+    
+
+chai = chai_customer()
+next(chai) # executes the code of lines till it meets yield.
+
+chai.send("Masala chai") # sends value to the yield...
+chai.send("Ginger chai") # sends value to the yield...
+
