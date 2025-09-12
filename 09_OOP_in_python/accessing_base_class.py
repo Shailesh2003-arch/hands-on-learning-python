116ab2d9f4c2586fa2506b20c44204d221a2e686
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-12 23:14:15 +0530

    add ways to access base class

---
+++ 09_OOP_in_python/accessing_base_class.py
@@ -0,0 +1,73 @@
+# This file will contain all the knowledge about how to access base class...
+
+# Code duplication
+# Explicit call
+# super()
+
+class Chai:
+    def __init__(self, type_, strength):
+        self.type = type_
+        self.strength = strength
+        print("Constructor of base Class executed")
+
+    def printChaiDetails(self):
+        return(f"Serving {self.type} with {self.strength} strength")
+
+# class GingerChai(Chai): # inherited a base class Chai...
+#     def __init__(self,type_,strength,spice_level): 
+        
+#     # to acccess the methods and variables of base class we need to execute the constructor of base class from the child class, but here we're probably not doing it, as we're doing code duplication and nothing else...
+        
+#         self.type = type_
+#         self.strength = strength
+#         self.spice_level = spice_level
+
+    def serve(self):
+        return f"Serving {self.type} with {self.strength} strength and {self.spice_level} spice level"
+
+
+
+# my_chai = Chai("Masala Chai","Strong")
+# print(my_chai.printChaiDetails())
+
+# my_chai_with_strength = GingerChai("Masala Chai","Strong","Mild")
+# print(my_chai_with_strength.serve())
+
+
+# Second way of accessing the base class using explicit call to the base class's constructor...
+
+
+# class GingerChai(Chai): # inherited a base class Chai...
+#     def __init__(self,type_,strength,spice_level): 
+#         Chai.__init__(self,type_, strength) # making an explicit call to base class's constructor...
+#         self.spice_level = spice_level
+    
+#     def printChaiDetails(self):
+#         return(f"Serving {self.type} with {self.strength} strength")
+
+# my_chai_with_strength = GingerChai("Masala Chai","Strong","Mild")
+# print(my_chai_with_strength.serve())
+
+
+
+# Third way of accessing the base class using super().
+
+class GingerChai(Chai): # inherited a base class Chai...
+    def __init__(self, type_, strength, spice_level):
+        super().__init__(type_, strength)
+        self.spice_level = spice_level
+    
+    def printChaiDetails(self):
+        return(f"Serving {self.type} with {self.strength} strength")
+
+my_chai_with_strength = GingerChai("Masala Chai","Strong","Mild")
+print(my_chai_with_strength.serve())
+
+
+
+      
+
+
+
+
+        
