f88a19da932472d3788f77d15848f713f12327a8
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-17 20:08:41 +0530

    add description of nested model

---
+++ 13_pydantic/models/nested_models/nested_models.py
@@ -0,0 +1,3 @@
+# This file will contain the knowledge about nested model in pydantic.
+# Nested model allows you to compose complex data structure by embedding one pydantic model inside other pydantic model.
+
