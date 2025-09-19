a960eee9fe80b95b2a03b2f9716aa81a2c5907e7
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-19 11:38:45 +0530

    add nested models into the codebase

--- 13_pydantic/models/nested_models/nested_models.py
+++ 13_pydantic/models/nested_models/nested_models.py
@@ -1,3 +1,31 @@
 # This file will contain the knowledge about nested model in pydantic.
 # Nested model allows you to compose complex data structure by embedding one pydantic model inside other pydantic model.
 
+from pydantic import BaseModel, ValidationError
+from typing import List, Optional
+
+
+class Address(BaseModel):
+    street:str
+    city:str
+    zipcode:str
+
+
+class User(BaseModel):
+    id:int
+    name:str
+    address:Address
+
+
+address = {
+    'street':"Luminous street",
+    'city':"London",
+    'zipcode':"431001"
+}
+
+
+try:
+    user = User(id=101, name="Shailesh", address=address)
+    print(user)
+except ValidationError as e:
+    print(str(e))
