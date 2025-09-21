69e0bc0b5d27468307cdcbdd91e7beb9c0e0f7fb
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-21 21:39:33 +0530

    add demonstration of using LLM (genAI)

---
+++ 14_Core_foundation_of_generative_AI/02_using_LLMs/helloLLMs.py
@@ -0,0 +1,9 @@
+# from google import genai
+
+# client = genai.Client()
+
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash", contents="Tell me about yourself in a little detailed way like your name, when and who made you?"
+# )
+
+# print(response.text)
